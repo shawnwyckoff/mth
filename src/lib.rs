be1df96a48830ec1b51c705ee02c205dba18a98d
shawnwyckoff <shawnwyckoff@protonmail.com>
2020-07-29 12:47:44 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+pub mod basic;
+pub mod decimal;
+pub mod hex;
+pub mod stat;
+pub mod time_series;
+pub mod matrix;
+pub mod ornstein_uhlenbeck_process;
+pub mod probability_distribution;
+pub mod vandermonde_matrix;
+
 #[cfg(test)]
 mod tests {
     #[test]
